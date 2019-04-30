d58c97881823710d76e2ec16defde1d4cbe7b8e5
Alaa Gnadrey <alaa.gnadrey@toluna.com>
2019-04-30 15:32:33 +0300

    print data as json

--- resources/executors/sql_executer.py
+++ resources/executors/sql_executer.py
@@ -9,7 +9,7 @@ def main():
     con=psycopg2.connect(dbname= 'crossensedb', host='redshift-cluster-2.c4srspvxpvhg.eu-west-1.redshift.amazonaws.com', 
     port= '5439', user= 'crossense', password= 'txKW9hkN4kKN4aoP')
     cur = con.cursor()
-    cur.execute("select domain_name from main.mobile_urls limit 1;")
+    cur.execute("select id from main.mobile_urls limit 1;")
     data = cur.fetchall()
 
     if data and len(data):
@@ -22,5 +22,5 @@ def main():
     con.close()
 
 if __name__ == "__main__":
-    print("sql exectuer main")
+    print("sql exectuer main")  
     main()
