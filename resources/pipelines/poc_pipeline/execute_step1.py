5290e2b9967d10e36d98bad56cd6207f83a5a79e
Alaa Gnadrey <alaa.gnadrey@toluna.com>
2019-05-02 08:58:42 +0300

    fixed pipeline steps

--- resources/pipelines/poc_pipeline/execute_step1.py
+++ resources/pipelines/poc_pipeline/execute_step1.py
@@ -6,8 +6,8 @@ output_path = "resources/pipelines/poc_pipeline/"
 output_file_path = output_path + 'test1.txt'
 
 update_query="update israel_team.users_amazon_score_by_action_poc set amount_of_days=45 WHERE respondent_id='7ee87a52-cbf3-4ba7-af74-d0d267d9bba4' and action_name='Search'" 
-con=psycopg2.connect(dbname= 'crossensedb', host='redshift-cluster-2.c4srspvxpvhg.eu-west-1.redshift.amazonaws.com', 
-    port= '5439', user= 'crossense', password= 'txKW9hkN4kKN4aoP')
+con=psycopg2.connect(dbname= 'crossensedb', host='xs-redshift-instance.cm17e00im9n9.us-east-1.redshift.amazonaws.com', 
+    port= '5439', user= 'crossense', password= '89VdfK5RmWKzUw43')
 cur = con.cursor()
 cur.execute(update_query)
 
